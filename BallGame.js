772fbfa2475ab1b5d2716093598b3ec908302d02
German Pustovojtovskij <g.pustovojtovskij@weisenburger.de>
2020-07-17 16:20:51 +0200

    Ball moving

--- BallGame.js
+++ BallGame.js
@@ -1,26 +1,22 @@
 function deviceOrientationListener(event) {
     var ball = document.getElementById("ball");
-    var garden = document.getElementById("garden");
 
-    var maxX = garden.clientWidth - ball.clientWidth;
-    var maxY = garden.clientHeight - ball.clientHeight;
+    var maxX = screen.width;
+    var maxY = screen.height;
 
     var x = event.gamma;
     var y = event.beta;
+    var positionX = ((x / 90) * maxX) + (maxX / 2);
+    var positionY = ((y / 90) * maxY) + (maxY / 2);
 
-    if (x > 90) {
-        x = 90
+    if (positionX > 0 && positionX < maxX) {
+        ball.style.top = positionX + "px";
+    }
+    if (positionY > 0 && positionY < maxY) {
+        ball.style.left = positionY + "px";
     }
 
-    if (x < -90) {
-        x = -90
-    }
 
-    x += 90;
-    y += 90;
-
-    ball.style.top = (maxY * y * 1.2 / 180 - 10) + "px";
-    ball.style.left = (maxX * x * 1.2 / 180 - 10) + "px";
 }
 
 window.addEventListener('deviceorientation', deviceOrientationListener);
