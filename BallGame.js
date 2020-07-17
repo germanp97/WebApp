85c732b6f3cd2969c3c0391dc95b090df99c9dbd
German Pustovojtovskij <g.pustovojtovskij@weisenburger.de>
2020-07-17 12:50:42 +0200

    Device Orientation

--- BallGame.js
+++ BallGame.js
@@ -3,13 +3,35 @@ if (window.DeviceOrientationEvent) {
 } else {
     document.getElementById("headline").innerText = "test"
 }
+var garden = document.getElementById("garden");
+var ball = document.getElementById("ball");
+
+var maxX = garden.clientWidth - ball.clientWidth;
+var maxY = garden.clientHeight - ball.clientHeight;
+
 
 function deviceOrientationListener(event) {
-    var a = event.alpha;
-    var b = event.beta;
-    var c = event.gamma;
-    document.getElementById("alpha").innerText = a;
-    document.getElementById("beta").innerText = b;
-    document.getElementById("gamma").innerText = c;
+    // var a = event.alpha;
+    // var b = event.beta;
+    // var c = event.gamma;
+    // document.getElementById("alpha").innerText = a;
+    // document.getElementById("beta").innerText = b;
+    // document.getElementById("gamma").innerText = c;
 
+    var x = event.beta;
+    var y = event.gamma;
+
+    if (x > 90) {
+        x = 90
+    }
+
+    if (x < -90) {
+        x = -90
+    }
+
+    x += 90;
+    y += 90;
+
+    ball.style.top = (maxY * y / 180 - 10) + "px";
+    ball.style.left = (maxX * x / 180 - 10) + "px";
 }
