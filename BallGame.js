8cff2ee6adca405347bbf99d8ae155a6d8fadd3e
German Pustovojtovskij <g.pustovojtovskij@weisenburger.de>
2020-07-19 17:15:56 +0200

    Pong with good orientation

--- BallGame.js
+++ BallGame.js
@@ -1,9 +1,10 @@
 function deviceOrientationListener(event) {
     const bar = document.getElementById('bar');
     var maxX = screen.width;
+    var boundary = maxX - 64;
     const gammaAxis = event.gamma;
     const position = ((gammaAxis / 90) * maxX) + (maxX / 2)
-    if (position > 0 && (position) < maxX) {
+    if (position > 0 && (position) < boundary) {
         bar.style.left = position + 'px';
     }
 }
