fecc37de63d7b7328082bef3d97d4f97b969a637
German Pustovojtovskij <g.pustovojtovskij@weisenburger.de>
2020-08-29 11:08:17 +0200

    editing ball

--- BallGame.js
+++ BallGame.js
@@ -72,9 +72,9 @@
 //     context.clearRect(0, 0, canvas.width, canvas.height);
 //     document.getElementById("bar").style.display = "none";
 //     document.getElementById("startGame").style.display = "inline-block";
-//     document.getElementById("final-score").style.display = "inline";
+//     document.getElementById("description").style.display = "inline";
 //     document.getElementById("startGame").innerHTML = "Let's go Again!"
-//     document.getElementById("final-score").innerHTML = "Your final score is: " + score
+//     document.getElementById("description").innerHTML = "Your final score is: " + score
 //     document.getElementById("score").style.display = "none";
 // }
 //
@@ -115,7 +115,7 @@ function startGame() {
     // dx = 3;
     console.log(document.getElementById("startGame").style);
     document.getElementById("startGame").style.display = "none";
-    document.getElementById("final-score").style.display = "none";
+    document.getElementById("description").style.display = "none";
     intervalCounter = setInterval(timeIt, 1000);
 }
 
@@ -132,8 +132,8 @@ function timeIt() {
 
 document.addEventListener('DOMContentLoaded', function () {
     canvas = document.getElementById("myCanvas");
-    canvas.width = screen.width - 100;
-    canvas.height = screen.height * 0.75;
+    canvas.width = screen.width - 70;
+    canvas.height = screen.height * 0.832;
     ctx = canvas.getContext("2d");
     x = canvas.width / 2;
     y = canvas.height - 30;
