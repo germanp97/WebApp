b4079decc7ac4e13d4ff62d466252707ed33ab94
German Pustovojtovskij <g.pustovojtovskij@weisenburger.de>
2020-08-29 12:51:40 +0200

    refactor

--- BallGame.js
+++ BallGame.js
@@ -92,7 +92,7 @@ function collisionDetection() {
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = "#00c3dd";
     ctx.fill();
     ctx.closePath();
 }
@@ -100,7 +100,7 @@ function drawBall() {
 function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = "#00c3dd";
     ctx.fill();
     ctx.closePath();
 }
@@ -115,7 +115,7 @@ function drawBricks() {
                 bricks[c][r].y = brickY;
                 ctx.beginPath();
                 ctx.rect(brickX, brickY, brickWidth, brickHeight);
-                ctx.fillStyle = "#0095DD";
+                ctx.fillStyle = "#00c3dd";
                 ctx.fill();
                 ctx.closePath();
             }
@@ -125,7 +125,7 @@ function drawBricks() {
 
 function drawScore() {
     ctx.font = "16px Arial";
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = "#000000";
     ctx.fillText("Score: " + score, 8, 20);
 }
 
